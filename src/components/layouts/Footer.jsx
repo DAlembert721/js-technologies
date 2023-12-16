d1eb60722d944e1710bda603e43a4832367a23ab
elliotgaramendi <elliotgaramendi@gmail.com>
2023-12-16 13:46:25 -0500

    feat: Create Footer component

---
+++ src/components/layouts/Footer.jsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <footer className="mt-auto bg-sky-500">
+      <nav className="py-4">
+        <div className="container px-4 mx-auto">
+          <a href="https://www.linkedin.com/in/elliotgaramendi/">
+            <h2
+              className="text-zinc-100 font-extrabold text-center transition-all hover:brightness-125"
+            >
+              Made with ♥ by Elliot Garamendi.
+            </h2>
+          </a>
+        </div>
+      </nav>
+    </footer>
+  );
+};
+
+export default Footer;
